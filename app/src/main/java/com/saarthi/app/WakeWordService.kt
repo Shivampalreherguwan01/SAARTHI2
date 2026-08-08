c78bd533dc074730b6008c576f6be4d96b7e06fb
Shivam <palchanda467@gmail.com>
2026-08-08 16:45:10 +0530

    Revert grammar constraint - was breaking recognition

--- app/src/main/java/com/saarthi/app/WakeWordService.kt
+++ app/src/main/java/com/saarthi/app/WakeWordService.kt
@@ -151,11 +151,9 @@ class WakeWordService : Service(), RecognitionListener {
         startForeground(1, notification)
     }
 
-    private val grammar = "[\"saarthi\", \"sarthi\", \"sarathi\", \"[unk]\"]"
-
     private fun startListening() {
         try {
-            val rec = Recognizer(model, 16000.0f, grammar)
+            val rec = Recognizer(model, 16000.0f)
             speechService = SpeechService(rec, 16000.0f)
             speechService?.startListening(this)
             updateNotification("Sun raha hoon...")
